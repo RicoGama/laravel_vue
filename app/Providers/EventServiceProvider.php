76cb36ab739a7a587b901d0e7477b880ef2d3bc4
Ricardo Gama <ricard.gama@gmail.com>
2016-12-30 10:53:59 -0200

    Teestando atribuicao de conta bancaria padrao

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,9 +3,12 @@
 namespace CodeFin\Providers;
 
 use CodeFin\Events\BankStoredEvent;
+use CodeFin\Listeners\BankAccountSetDefaultListener;
 use CodeFin\Listeners\BankLogoUploadListener;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Prettus\Repository\Events\RepositoryEntityCreated;
+use Prettus\Repository\Events\RepositoryEntityUpdated;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -17,6 +20,12 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         BankStoredEvent::class => [
             BankLogoUploadListener::class
+        ],
+        RepositoryEntityCreated::class => [
+            BankAccountSetDefaultListener::class
+        ],
+        RepositoryEntityUpdated::class => [
+            BankAccountSetDefaultListener::class
         ]
     ];
 
