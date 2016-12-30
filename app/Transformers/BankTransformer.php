d253a1bbfe05edf848394d16bf7bb871248d2ca3
Ricardo Gama <ricard.gama@gmail.com>
2016-12-30 11:34:56 -0200

    Serializando link para logo do banco

--- app/Transformers/BankTransformer.php
+++ app/Transformers/BankTransformer.php
@@ -23,8 +23,16 @@ class BankTransformer extends TransformerAbstract
         return [
             'id'         => (int) $model->id,
             'name'       => $model->name,
+            'logo'       => $this->makeLogoPath($model),
             'created_at' => $model->created_at,
             'updated_at' => $model->updated_at
         ];
+    }
+
+    public function makeLogoPath(Bank $model)
+    {
+        $url = url('/');
+        $folder = Bank::logosDir();
+        return "$url/storage/$folder/{$model->logo}";
     }
 }
