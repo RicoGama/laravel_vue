4062778086e613a184b397eea36949f2a89e3dda
Ricardo Gama <ricard.gama@gmail.com>
2017-01-12 09:36:08 -0200

    WatchIgnore plugin

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,8 +1,11 @@
 var webpack = require('webpack');
-
+var path = require('path');
 module.exports = {
     devtool: 'source-map',
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.WatchIgnorePlugin([
+            path.resolve(__dirname, './node_modules/')
+        ])
     ]
 };
