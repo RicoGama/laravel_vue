a44ab569c7f5f30f578d6123b5e20cdd5133450d
Ricardo Gama <ricard.gama@gmail.com>
2017-01-12 10:28:21 -0200

    Criando tabela de categorias

---
+++ app/Repositories/CategoryRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CodeFin\Repositories;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface CategoryRepository
+ * @package namespace CodeFin\Repositories;
+ */
+interface CategoryRepository extends RepositoryInterface
+{
+    //
+}
---
+++ app/Repositories/CategoryRepositoryEloquent.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace CodeFin\Repositories;
+
+use Prettus\Repository\Eloquent\BaseRepository;
+use Prettus\Repository\Criteria\RequestCriteria;
+use CodeFin\Repositories\CategoryRepository;
+use CodeFin\Models\Category;
+use CodeFin\Validators\CategoryValidator;
+
+/**
+ * Class CategoryRepositoryEloquent
+ * @package namespace CodeFin\Repositories;
+ */
+class CategoryRepositoryEloquent extends BaseRepository implements CategoryRepository
+{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return Category::class;
+    }
+
+    
+
+    /**
+     * Boot up the repository, pushing criteria
+     */
+    public function boot()
+    {
+        $this->pushCriteria(app(RequestCriteria::class));
+    }
+}
