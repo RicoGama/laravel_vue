4d9eda81fca4e0a3b3384d173c20dc0e5398b3cf
Ricardo Gama <ricard.gama@gmail.com>
2017-01-19 12:07:30 -0200

    Criando acoes para categorias

--- resources/assets/spa/js/components/category/CategoryTree.vue
+++ resources/assets/spa/js/components/category/CategoryTree.vue
@@ -2,10 +2,21 @@
     <ul class="category-tree">
         <li v-for="(index, o) in categories" class="category-child">
             <div class="valign-wrapper">
-                <a href="#" class="category-symbol"
+                <a :data-activates="dropdownId(o)" href="#" class="category-symbol"
                    :class="{'green-text' : o.children.data.length > 0, 'grey-text': !o.children.data.length}">
                     <i class="material-icons">{{ categoryIcon(o) }}</i>
                 </a>
+                <ul :id="dropdownId(o)" class="dropdown-content">
+                    <li>
+                        <a href="#">Adicionar</a>
+                    </li>
+                    <li>
+                        <a href="#">Editar</a>
+                    </li>
+                    <li>
+                        <a href="#">Excluir</a>
+                    </li>
+                </ul>
                 <span class="valign">{{{ categoryText(o) }}}</span>
             </div>
             <category-tree :categories="o.children.data"></category-tree>
@@ -22,7 +33,23 @@
                 required: true
             }
         },
+        watch: {
+            categories: {
+                handler(categories) {
+                    $('.category-child > div > a').dropdown({
+                        hover: true,
+                        inDuration: 300,
+                        outDuration: 400,
+                        belowOrigin: true
+                    });
+                },
+                deep: true
+            }
+        },
         methods: {
+            dropdownId(category) {
+                return `category-tree-dropdown-${category.id}`;
+            },
             categoryText(category) {
                 return category.children.data.length > 0 ? `<strong>${category.name}</strong>` : category.name;
             },
