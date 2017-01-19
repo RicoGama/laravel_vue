285e0146af0dfc8ce285116cd5e7031c7179fc92
Ricardo Gama <ricard.gama@gmail.com>
2017-01-19 11:38:53 -0200

    Estilizando categorias

--- resources/assets/spa/js/components/category/CategoryTree.vue
+++ resources/assets/spa/js/components/category/CategoryTree.vue
@@ -1,7 +1,12 @@
 <template>
-    <ul>
-        <li v-for="(index, o) in categories">
-            {{ o.name }}
+    <ul class="category-tree">
+        <li v-for="(index, o) in categories" class="category-child">
+            <div class="valign-wrapper">
+                <a href="#" class="category-symbol green-text">
+                    <i class="material-icons">folder</i>
+                </a>
+                <span class="valign">{{ o.name }}</span>
+            </div>
             <category-tree :categories="o.children.data"></category-tree>
         </li>
     </ul>
