ebbc0c5b5483e2d696d582ed2205c406e8d5c5e6
Ricardo Gama <ricard.gama@gmail.com>
2017-02-02 13:06:16 -0200

    Criando logica para edicao de categorias

--- resources/assets/spa/js/services/category-nsm.js
+++ resources/assets/spa/js/services/category-nsm.js
@@ -45,4 +45,63 @@ export class CategoryService {
             return response;
         });
     }
+
+    static edit(category, parent, categories, categoryOriginal) {
+        let categoryCopy = $.extend(true, {}, category);
+        if (categoryCopy.parent_id === null) {
+            delete categoryCopy.parent_id;
+        }
+        let self = this;
+        return Category.update({id: categoryCopy.id}, categoryCopy).then(response => {
+            let categoryUpdated = response.data.data;
+            if (categoryUpdated.parent_id === null) {
+                /**
+                 * Categoria alterada, está sem pai
+                 * E antes ela tinha um pai
+                 */
+                if (parent) {
+                    parent.children.data.$remove(categoryOriginal);
+                    categories.push(categoryUpdated);
+                    return response;
+                }
+            } else {
+                /**
+                 * Categoria alterada, tem pai
+                 */
+                if (parent) {
+                    /**
+                     * Trocar categoria de pai
+                     */
+                    if (parent_id != categoryUpdated.parent_id) {
+                        parent.children.data.$remove(categoryOriginal);
+                        self._addChild(categoryUpdated, categories);
+                        return response;
+                    }
+                } else {
+                    /**
+                     * Tornar a categoria um filho
+                     * Antes a categoria era um pai
+                     */
+                    categories.$remove(categoryOriginal);
+                    self._addChild(categoryUpdated, categories);
+                    return response;
+                }
+            }
+            return response;
+        });
+    }
+
+    static _addChild(child, categories) {
+        let parent = this._findParent(child.parent_id, categories);
+        parent.children.data.push(child);
+    }
+
+    static _findParent(id, categories) {
+        for (let category of categories) {
+            if (id == category.id) {
+                return category;
+            }
+            return this._findParent(id, category.children.data);
+        }
+    }
 }
