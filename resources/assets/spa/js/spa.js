709c7fd0accc3ece22c09718a8a3a2a8b185e879
Ricardo Gama <ricard.gama@gmail.com>
2017-02-25 08:52:26 -0300

    Criando filtro pra data

--- resources/assets/spa/js/spa.js
+++ resources/assets/spa/js/spa.js
@@ -1,13 +1,12 @@
 import appConfig from './services/appConfig';
-//import Vuex from 'vuex';
 
 require('materialize-css');
 window.Vue = require('vue');
-//window.Vue.use(Vuex);
 require('vue-resource');
 require('vuex');
 Vue.http.options.root = appConfig.api_url;
 
+require('./filters');
 require('./services/interceptors');
 require('./router');
 
