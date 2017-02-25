1e687f760849c0c3327e59d4a6ea3ee55d384bce
Ricardo Gama <ricard.gama@gmail.com>
2017-02-25 09:35:16 -0300

    Traduzindo validacao

--- resources/assets/spa/js/validators.js
+++ resources/assets/spa/js/validators.js
@@ -1,3 +1,11 @@
 import VeeValidate from 'vee-validate';
+import dictPtMessages from './locale/validator/pt-br';
 
-Vue.use(VeeValidate);
+Vue.use(VeeValidate, {
+    locale: 'pt-br',
+    dictionary: {
+        'pt-br': {
+            messages: dictPtMessages
+        }
+    }
+});
