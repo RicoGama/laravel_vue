f8621a1ccef59201a2f4669a73d893a256822b83
Ricardo Gama <ricard.gama@gmail.com>
2017-02-25 22:22:34 -0300

    Organizando campos de repeticao

--- resources/assets/spa/js/mixins/bill-mixin.js
+++ resources/assets/spa/js/mixins/bill-mixin.js
@@ -29,6 +29,9 @@ export default {
                 bank_account_id: '',
                 category_id: 0
             },
+            repeat: false,
+            repeat_type: 1,
+            repeat_number: 0,
             bankAccount: {
                 name: '', text: ''
             }
@@ -78,6 +81,9 @@ export default {
         formId() {
             return `form-bill-${this._uid}`;
         },
+        repeatId() {
+            return `repeat-${this._uid}`;
+        },
         blurBankAccount($event) {
             let el = $($event.target);
             let text = this.bankAccount.text;
@@ -85,6 +91,12 @@ export default {
                 el.val(text);
             }
             this.validateBankAccount();
+        },
+        blurRepeatNumber($event) {
+            let el = $($event.target);
+            if (parseInt(el.val(), 10) < 0) {
+                el.val(0);
+            }
         },
         validateCategory() {
             let valid = this.$validator.validate('category_id', this.bill.category_id);
@@ -161,6 +173,9 @@ export default {
                 bank_account_id: '',
                 category_id: 0
             }
+            this.repeat = false;
+            this.repeat_type = 1;
+            this.repeat_number = 0;
         }
     }
 }
