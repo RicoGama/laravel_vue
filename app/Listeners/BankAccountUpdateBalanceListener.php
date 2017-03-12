b4a03f34c590f51d050f3fc746d268b8e12a89cb
Ricardo Gama <ricard.gama@gmail.com>
2017-03-12 00:09:39 -0300

    Listener para alterar saldo e extrato

--- app/Listeners/BankAccountUpdateBalanceListener.php
+++ app/Listeners/BankAccountUpdateBalanceListener.php
@@ -3,6 +3,7 @@
 namespace CodeFin\Listeners;
 
 use CodeFin\Events\BillStoredEvent;
+use CodeFin\Models\BillPay;
 use CodeFin\Repositories\BankAccountRepository;
 use CodeFin\Repositories\StatementRepository;
 use Prettus\Repository\Events\RepositoryEventBase;
@@ -62,6 +63,26 @@ class BankAccountUpdateBalanceListener
 
     protected function getValue(BillStoredEvent $event)
     {
+        $model = $event->getModel();
+        $modelOld = $event->getModelOld();
 
+        $value = $model->value;
+        $valueOld = $modelOld ? $modelOld->value : $model->value;
+        $doneOld = $modelOld ? $modelOld->done : false;
+
+        if ($value != $valueOld) { // valor do conta alterado
+            if ($model->done == $modelOld->done && $model->done) { // conta continua paga
+                return $model instanceof BillPay ? $valueOld - $value : $value - $valueOld;
+            }
+        }
+
+        if ($model->done != $doneOld) {
+            if ($model->done) {
+                return $model instanceof BillPay ? -$value : $value;
+            } else {
+                return $model instanceof BillPay ? $valueOld : -$valueOld;
+            }
+        }
+        return 0;
     }
 }
